da4cadcc9003af3a07f0affaa7dc565cf9acc967
danijrey <56493697+danijrey@users.noreply.github.com>
2020-05-22 13:04:10 -0500

    Listo el para Pull sin node_modules

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const initDatabase = require('./src/db.js');
 const providerController = require('./src/controllers/provider.controller.js');
-const { auth } = require ('./src/utils/middlewares.js')
+const providerRouter = require('./src/routes/provider.js');
 
 
 initDatabase();
@@ -11,15 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/providers', providerRouter);
 
-app.get('/', (req,res) => providerController);
-
-//Esto se pasa a cada una de las rutas para acceder al controlador
-app.get('/providers',providerController.all);
-app.post('/providers',providerController.create);
-app.get('/providers/:id',providerController.show);
-app.put('/providers/:id',providerController.edit);
-app.delete('/providers/:id',providerController.destroy);
 
 
 app.listen(8080, () => console.log(`App running on http://localhost:8080`));
