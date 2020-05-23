42ac8266c2297c3797757c97506fe7c4a693a7c3
danijrey <56493697+danijrey@users.noreply.github.com>
2020-05-23 07:13:14 -0500

    Servidor de Providers-Branch creado y testeado desde Postman

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const mongoose = require("mongoose");
 const initDatabase = require('./src/db.js');
 const providerController = require('./src/controllers/provider.controller.js');
 const providerRouter = require('./src/routes/provider.js');
-
+const branchController = require('./src/controllers/branch.controller.js');
+const branchRouter = require('./src/routes/branch.js');
 
 initDatabase();
 const app = express();
@@ -13,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/providers', providerRouter);
-
+app.use('/providers/:id/branch', branchRouter);
 
 
 app.listen(8080, () => console.log(`App running on http://localhost:8080`));
