9669372996c0134402b8f196cdc1ec9669b48eb3
danijrey <56493697+danijrey@users.noreply.github.com>
2020-05-28 19:42:13 -0500

    Branch server (#9)
    * Servidor de Providers-Branch creado y testeado desde Postman
    
    * Listo servidor Providers-Client-LogIn-Branch
    
    * Realizo cambios en forma y Sintaxis del Server. Todos las conexiones funcionan apropiadamente
    
    * Listos cambios del PR
    
    * Listo el PR sin el archivo git

--- server.js
+++ server.js
@@ -3,17 +3,25 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const initDatabase = require('./src/db.js');
+const clientController = require('./src/controllers/client.controller.js');
 const providerController = require('./src/controllers/provider.controller.js');
+const branchController = require('./src/controllers/branch.controller.js');
+const loginController = require('./src/controllers/login.controller.js');
+const clientRouter = require('./src/routes/client.js');
 const providerRouter = require('./src/routes/provider.js');
-
+const branchRouter = require('./src/routes/branch.js');
+const loginRouter = require ('./src/routes/login.js');
+const { auth } = require ('./src/utils/middlewares.js');
 
 initDatabase();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/', loginRouter);
+app.use('/clients', clientRouter);
 app.use('/providers', providerRouter);
-
+app.use('/providers/:id/branch', branchRouter);
 
 
 app.listen(8080, () => console.log(`App running on http://localhost:8080`));
--- src/controllers/provider.controller.js
+++ src/controllers/provider.controller.js
@@ -3,56 +3,65 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 module.exports = {
-  all(req, res) {
-    Provider
-      .find()
-      .then((provider) => res.status(200).json(provider))
-      .catch((error) => res.status(500).json(error));
+  async all(req, res) {
+    try{
+      const provider = await Provider.find()
+      res.status(200).json(provider);
+      }
+    catch (error) {
+        console.log(error);
+      }
   },
   async create(req, res) {
-
     try {
       const data = req.body;
       const password = await bcrypt.hash(data.password, 8);
-      const provider = await Provider.create(data);
-
+      const provider = await Provider.create({providerEmail: data.providerEmail, password});
       const token = jwt.sign(
         { id: provider._id },
-        'holacarebola',
+        process.env.SECRET,
         { expiresIn: 60 * 60 * 24 * 365 }
         );
-
       res.status(200).json({ token });
-    }
+      }
     catch (error) {
       console.log(error);
     }
   },
-  show(req, res){
-    const { id }=req.params;
-    Provider
-     .findById(id)
-     .then((provider) => res.status(200).json(provider))
-     .catch((error) => res.status(400).json(error))
-  },
-  edit(req, res){
-    const { id } = req.params;
-    const data = req.body;
-    const options = {
-      new: true,
-      useFindAndModify: false
-    };
-    Provider
-      .findByIdAndUpdate(id, data, options)
-      .then((provider) => res.status(200).json(provider))
-      .catch((error) => res.status(400).json(error))
-  },
-  destroy(req, res) {
-    const { id } = req.params;
+  async show(req, res){
 
-    Provider
-      .findByIdAndDelete(id)
-      .then((provider) => res.status(200).json(provider))
-      .catch((error) => res.status(400).json(error));
-  }
-}
+    try{
+       const { id } = req.params;
+       const provider = await Provider.findById(id)
+       res.status(200).json(provider);
+      }
+    catch (error) {
+       console.log(error);
+     }
+  },
+  async edit(req, res){
+    try{
+      const { id } = req.params;
+      const data = req.body;
+      const options = {
+        new: true,
+        useFindAndModify: false
+        };
+      const provider = await Provider.findByIdAndUpdate(id, data, options)
+      res.status(200).json(provider);
+      }
+    catch (error) {
+      console.log(error);
+      }
+  },
+  async destroy(req, res) {
+    try{
+      const { id } = req.params;
+      const provider = await Provider.findByIdAndDelete(id)
+      res.status(200).json(provider);
+      }
+    catch (error) {
+        console.log(error);
+      }
+    }
+  };
