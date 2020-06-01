992f6d01753434013024d0d59c204970971f6323
danijrey <56493697+danijrey@users.noreply.github.com>
2020-06-01 11:20:40 -0500

    Lista authenticación y products en el server

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const productListController = require('../controllers/productList.controller.js');
 const { auth } = require ('../utils/middlewares.js');
 
-router.route('/all').get(productListController.list);
+router.route('/all').get(auth, productListController.list);
 router.route('/create').post(productListController.create);
 router.route('/show/:id').get(productListController.show);
 router.route('/edit/:id').put(productListController.edit);
 router.route('/destroy/:id').delete(productListController.destroy);
-// router.route('/home').get(auth, productListController.list);
+
 module.exports = router;
