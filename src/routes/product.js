fbd83e235b91eec02193e3ce7a3d3304a74b7832
SaraMmejia <sarammejia.11@gmail.com>
2020-06-02 10:41:51 -0500

    Cambios después de clonar el Repo

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,17 +1,15 @@
-const router = require('express').Router();
-const productListController = require('../controllers/productList.controller.js');
-const { auth } = require ('../utils/middlewares.js');
+// const router = require("express").Router();
+// const productListController = require("../controllers/productList.controller.js");
+// const { auth } = require("../utils/middlewares.js");
 
-router.route('/all').get(auth, productListController.list);
-router.route('/create').post(productListController.create);
-router.route('/show/:id').get(productListController.show);
-router.route('/edit/:id').put(productListController.edit);
-router.route('/destroy/:id').delete(productListController.destroy);
+// router.route("/all").get(auth, productListController.list);
+// router.route("/create").post(productListController.create);
+// router.route("/show/:id").get(productListController.show);
+// router.route("/edit/:id").put(productListController.edit);
+// router.route("/destroy/:id").delete(productListController.destroy);
 
-const loginController = require('../controllers/login.controller.js');
-const { auth } = require ('../utils/middlewares.js');
+// const loginController = require("../controllers/login.controller.js");
 
-router.route('/product').post(auth, productController.list);
+// router.route("/product").post(auth, productController.list);
 
-
-module.exports = router;
+// module.exports = router;
