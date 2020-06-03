06b23cbecde2e1ac145612fd1958d91b97284b62
danijrey <56493697+danijrey@users.noreply.github.com>
2020-06-03 09:57:27 -0500

    Lista validación client-provider del login (#13)

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
 const router = require("express").Router();
 const productController = require("../controllers/product.controller.js");
 const { auth } = require("../utils/middlewares.js");
 
-router.route("/all").get(productController.all);
+
+router.route("/all").get(auth, productController.all);
 router.route("/create").post(productController.create);
 router.route("/show/:id").get(productController.show);
 router.route("/edit/:id").put(productController.edit);
 router.route("/destroy/:id").delete(productController.destroy);
-=======
-const router = require('express').Router();
-const productController = require('../controllers/product.controller.js');
-const { auth } = require('../utils/middlewares.js');
-
-router.route('/all').get(productController.all);
-router.route('/create').post(productController.create);
-router.route('/show/:id').get(productController.show);
-router.route('/edit/:id').put(productController.edit);
-router.route('/destroy/:id').delete(productController.destroy);
->>>>>>> 4dbe76f02d21aaedf7ca94206e2ef134c79e96cc
 
 module.exports = router;
--- src/routes/provider.js
+++ src/routes/provider.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
 const providerController = require('../controllers/provider.controller.js');
-<<<<<<< HEAD
 const { auth } = require ('../utils/middlewares.js');
-=======
-const { auth } = require('../utils/middlewares.js');
->>>>>>> 4dbe76f02d21aaedf7ca94206e2ef134c79e96cc
 
-router.route('/all').get(providerController.all);
+
+router.route('/all').get(auth, providerController.all);
 router.route('/create').post(providerController.create);
 router.route('/show/:id').get(providerController.show);
 router.route('/edit/:id').put(providerController.edit);
