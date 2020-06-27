3f2a2897a7f7822c2a34f056becff85534dd2255
SaraMmejia <sarammejia.11@gmail.com>
2020-06-27 15:30:52 -0500

    Heroku

--- src/db.js
+++ src/db.js
@@ -1,19 +1,19 @@
 //simepre es el mismo código:
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 function initDatabase() {
   const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-};
-  
-mongoose.connect('mongodb://localhost:27017/database', options);
+  };
 
-const { connection } = mongoose;
+  mongoose.connect(process.env.MONGO_URI, options);
 
-connection.once('open', () => console.log('Connection established succesfully'));
-connection.on('error', (err) => console.log('Something went wrong!', err));
+  const { connection } = mongoose;
+
+  connection.once("open", () =>
+    console.log("Connection established succesfully")
+  );
+  connection.on("error", (err) => console.log("Something went wrong!", err));
 }
 module.exports = initDatabase;
-
