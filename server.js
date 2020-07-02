4dfd2dd8b3663ac1b443dac3279a4394c374f50c
danijrey <56493697+danijrey@users.noreply.github.com>
2020-07-02 18:30:42 -0500

    Ultimo bug con Dev solucionado - Todo funcionando

--- server.js
+++ server.js
@@ -1,31 +1,8 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const initDatabase = require('./src/db.js');
-const clientRouter = require('./src/routes/client.js');
-const providerRouter = require('./src/routes/provider.js');
-const branchRouter = require('./src/routes/branch.js');
-const loginRouter = require('./src/routes/login.js');
-const productRouter = require('./src/routes/product.js');
-const searchRouter = require('./src/routes/search.js');
+const app = require('./src/app.js');
 
 const port = process.env.PORT || 8080;
 
-
-
-initDatabase();
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-
-app.use('/', loginRouter);
-app.use('/clients', clientRouter);
-app.use('/clients/search', searchRouter);
-app.use('/providers/search', searchRouter);
-app.use('/providers', providerRouter);
-app.use('/products', productRouter);
-app.use('/providers/:id/branch', branchRouter);
-
-app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server listening on http://localhost:${port}`)
+);
