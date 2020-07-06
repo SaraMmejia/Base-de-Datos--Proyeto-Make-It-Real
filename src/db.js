ca64e55d135d0f615afe34cb23dd2a5339fc256b
SaraMmejia <sarammejia.11@gmail.com>
2020-07-06 01:25:23 -0500

    Todo lo relacionado con carritos de compras

--- src/db.js
+++ src/db.js
@@ -1,21 +1,19 @@
 //simepre es el mismo código:
 const mongoose = require('mongoose');
 
-
 function initDatabase() {
-  const mongoURI = process.env.SERVER_URL;
   const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   };
-  
-  mongoose.connect(mongoURI, options);
 
   mongoose.connect(process.env.SERVER_URL, options);
 
   const { connection } = mongoose;
 
-  connection.once('open', () => console.log('Connection established succesfully'));
+  connection.once('open', () =>
+    console.log('Connection established succesfully')
+  );
   connection.on('error', (err) => console.log('Something went wrong!', err));
 }
 
